7178c73d6b7da7b615044bdc1d4f30cdcbf1c964
mumbaicatkittensjogeshwari-oss <mumbaicatkittensjogeshwari@gmail.com>
2026-09-06 02:57:53 +0530

    Add files via upload

--- frontend/lib/widgets/glass_card.dart
+++ frontend/lib/widgets/glass_card.dart
@@ -1,1 +1,44 @@
+import 'package:flutter/material.dart';
 
+class GlassCard extends StatelessWidget {
+  final Widget child;
+  final double? width;
+  final double? height;
+
+  const GlassCard({
+    super.key,
+    required this.child,
+    this.width,
+    this.height,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.05),
+        borderRadius: BorderRadius.circular(16),
+        border: Border.all(
+          color: Colors.white.withOpacity(0.08),
+          width: 1,
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.3),
+            blurRadius: 20,
+            spreadRadius: 2,
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(16),
+        child: BackdropFilter(
+          filter: null, // Will be applied by child
+          child: child,
+        ),
+      ),
+    );
+  }
+}
